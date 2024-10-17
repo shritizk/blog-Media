6a69feda3eb42f4336d4a74f507a037184cd7f08
shritiz kumar <106963205+shritizk@users.noreply.github.com>
2024-10-17 18:04:09 +0530

    " check "

--- src/router/userRouter/userRoutes.ts
+++ src/router/userRouter/userRoutes.ts
@@ -1,6 +1,9 @@
 import {Router , Response , Request} from 'express';
 export const router  = Router();
 
+
+//db 
+
 // here i am expecting that all the input validation are already done in front end it self 
 
 
@@ -9,28 +12,22 @@ interface Responsedata {
     response : Response
 }
 
-//error to be fixed in function 
-//its type is not working thats given to me by express - Response , custom interface is not worling as well evenif i put some data and say make it strig or someting like that ( have done some gpt as well but same as always )
-// its working for any type  only  
-//
+
 // login - sighup route
-router.post('/sighup', function( req : Request, res : Response) {
+router.post('/sighup',async  function( req : Request, res : Response) {
     const payload = req.body;
     // use data will incude 
     // email 
     // name 
     // password 
-    try {
-        return res.json({
-            msg : "done"
+    try { 
+        res.json({
+            msg : "done "
         })
-        // heere all the signup login will go 
-    }catch(e) {
-        // here all the error are going to be handled 
-        console.log(e);
-        return res.json({
-            msg : "something went wrong "
-        });
+    }catch { 
+        res.json({
+            msg : "done something wrong !!"
+        })
     }
 })
 // edit userData router 
